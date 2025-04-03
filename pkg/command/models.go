adca462cbd2561775fa5d08ee210f22280c815bf
Kyle Cupp <kyle@controlplane.com>
2025-04-03 10:23:43 -0400

    Sync changes from upstream

--- pkg/command/models.go
+++ pkg/command/models.go
@@ -158,7 +158,7 @@ type ExpandVolumeStatus struct {
 	Messages           []string                `json:"messages,omitempty"`
 	Stage              ExpandVolumeStatusStage `json:"stage,omitempty"`
 	ReplicaRestartedAt string                  `json:"replicaRestartedAt,omitempty"`
-	LockName           string                  `json:"lockName,omitempty"`
+	LockNames          []string                `json:"lockNames,omitempty"`
 }
 
 type DeleteVolumeSpec struct {
