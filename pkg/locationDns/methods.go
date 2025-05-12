fac7f1b1991c9835c0faf9e43657bf83bc67b8dc
Kyle Cupp <kyle@controlplane.com>
2025-05-12 12:26:11 -0400

    Sync changes from upstream

---
+++ pkg/locationDns/methods.go
@@ -0,0 +1,18 @@
+package locationDns
+
+import (
+	"fmt"
+	"time"
+)
+
+func (r LocationDnsMessage) WorkloadLink() string {
+	return fmt.Sprintf("/org/%s/gvc/%s/workload/%s", r.Org, r.Gvc, r.Workload)
+}
+
+func (r LocationDnsMessage) ParseTimestamp() time.Time {
+	t, err := time.Parse(time.RFC3339, r.Timestamp)
+	if err != nil {
+		return time.Time{}
+	}
+	return t
+}
